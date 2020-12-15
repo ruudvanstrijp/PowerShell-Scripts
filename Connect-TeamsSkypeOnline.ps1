e0d4061a00a5e2546e90efa920e02847a8999845
ruudvanstrijp <ruudvanstrijp@gmail.com>
2020-12-15 11:35:32 +0100

    First upload

---
+++ Connect-TeamsSkypeOnline.ps1
@@ -0,0 +1,42 @@
+$debug = $false
+
+Import-module MicrosoftTeams
+ 
+if($debug -like $true){
+    Write-Host "  DEBUG: Trying to connect to existing session..." -ForegroundColor DarkGray
+}
+$pssession = Get-PSSession -name SfbPowerShell* | Where-Object {$_.Availability -eq 1}
+if($pssession.count -eq 0){
+    if($debug -like $true){
+        Write-Host "  DEBUG: Connecting to Skype Online..." -ForegroundColor DarkGray
+    }
+    try{
+        $sfboSession = New-CsOnlineSession
+    }
+    Catch{
+        $errOutput = [PSCustomObject]@{
+            status = "failed"
+            error = $_.Exception.Message
+            step = "Connecting to Skype Online"
+            cmdlet = "New-CsOnlineSession"
+        }
+        Write-Output ( $errOutput | ConvertTo-Json)
+        exit
+    }
+    if($debug -like $true){
+        Write-Host "  DEBUG: Importing PS Session..." -ForegroundColor DarkGray
+    }
+    try{
+        Import-PSSession $sfboSession -AllowClobber
+    }
+    Catch{
+        $errOutput = [PSCustomObject]@{
+            status = "failed"
+            error = $_.Exception.Message
+            step = "Importing PS Session"
+            cmdlet = "Import-PSSession"
+        }
+        Write-Output ( $errOutput | ConvertTo-Json)
+        exit
+    }
+}
