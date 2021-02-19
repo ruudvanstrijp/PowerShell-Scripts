0d452b896df9a4f304744c15c094b76512e66dee
ruudvanstrijp <ruudvanstrijp@gmail.com>
2021-02-19 16:16:42 +0100

    Added optional removal of ProgramData folder

--- Remove-JabberSettings.ps1
+++ Remove-JabberSettings.ps1
@@ -1,5 +1,6 @@
 $LocalFiles = Test-Path $env:USERPROFILE'\AppData\Local\Cisco\Unified Communications\Jabber\'
 $RoamingFiles = Test-Path $env:USERPROFILE'\AppData\Roaming\Cisco\Unified Communications\Jabber\'
+$ProgramData = Test-Path 'C:\ProgramData\Cisco Systems\Cisco Jabber'
 
 If ($LocalFiles){
     Remove-Item -LiteralPath $env:USERPROFILE'\AppData\Local\Cisco\Unified Communications\Jabber\' -Force -Recurse
@@ -7,4 +8,11 @@ If ($LocalFiles){
 
 If ($RoamingFiles){
     Remove-Item -LiteralPath $env:USERPROFILE'\AppData\Roaming\Cisco\Unified Communications\Jabber\' -Force -Recurse
+}
+
+If ($ProgramData){
+    $confirmation = Read-Host "Remove ProgramData? [y/n]"
+    if ($confirmation -eq 'y') {
+        Remove-Item -LiteralPath 'C:\ProgramData\Cisco Systems\Cisco Jabber' -Force -Recurse
+    }
 }
